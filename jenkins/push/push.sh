60107475906b80f403128101c066a5f4d7e57896
sushantk8s <54070702+sushantk8s@users.noreply.github.com>
2023-04-12 00:54:26 +0530

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,8 +7,8 @@ echo "********************"
 IMAGE="maven-project"
 
 echo "** Logging in ***"
-docker login -u ricardoandre97 -p $PASS
+docker login -u sushantk8s -p $PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG ricardoandre97/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_TAG sushantk8s/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push ricardoandre97/$IMAGE:$BUILD_TAG
+docker push sushantk8s/$IMAGE:$BUILD_TAG
