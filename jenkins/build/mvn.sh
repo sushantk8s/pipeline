23d087fe19da411c10cdd5c1f7d84b9917793e7b
sushantk8s <54070702+sushantk8s@users.noreply.github.com>
2023-04-12 00:24:48 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:latest "$@"
+mvn package
