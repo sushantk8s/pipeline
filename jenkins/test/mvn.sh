ef0bde88d90fc42f6c201f4560567d36236bac42
sushantk8s <54070702+sushantk8s@users.noreply.github.com>
2023-04-12 00:33:21 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,5 @@ echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
 WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
-
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+cd /home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/java-app
+mvn test
